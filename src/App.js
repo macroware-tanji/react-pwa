1f121dab3552f331a46f8b25e95c88ffd4f7c3a9
Kunihiro Tanji <tanji@macroware.co.jp>
2021-11-20 03:17:40 +0900

    v0.0005

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component{
   render() {
     return (
       <div>
-        <h1>Hello React Pwa v0.0004</h1>
+        <h1>Hello React Pwa v0.0005</h1>
         {
           /*
         <ul>
